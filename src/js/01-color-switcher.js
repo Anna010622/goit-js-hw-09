117879fa698476fd66c4ed747f4a3ef06fb3e0b8
Anna Trifonova <3809322999@ukr.net>
2023-02-25 21:08:11 +0200

    fix task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startBtnEl = document.querySelector('[data-start]');
 const stopBtnEl = document.querySelector('[data-stop]');
 const bodyEl = document.querySelector('body');
+let timerId;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
